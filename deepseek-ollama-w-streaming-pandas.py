514daac2aae822dbb9e75eb0d03aedcf77d413bc
bestisblessed <tyler.durette@gmail.com>
2025-02-18 00:33:41 -0500

    pandas ollama local analysis

--- deepseek-ollama-w-streaming-pandas.py
+++ deepseek-ollama-w-streaming-pandas.py
@@ -3,8 +3,10 @@ import subprocess
 
 csv_file_1 = 'data/fighter_info.csv'
 csv_file_2 = 'data/event_data_sherdog.csv'
+df1 = pd.read_csv(csv_file_1)
+df2 = pd.read_csv(csv_file_2)
 input_text = (
-    f"Analyze the following CSV files: {csv_file_1} and {csv_file_2}. "
+    f"Analyze the following CSV files: {df1} and {df2}. "
     "List me all column names and general statistics for each dataset and how many missing values are in each column. "
     # "Extract key trends, patterns, averages, outliers, and insights from each dataset. "
     # "Once your analysis is complete, use these datasets and insights to answer any questions I ask. "
